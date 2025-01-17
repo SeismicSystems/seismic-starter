385b27278a40a0479628c653422c6bd5f1fed9c0
Ameya Deshmukh <ad@seismic.systems>
2025-01-17 12:02:48 -0500

    looks ok now

--- packages/frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import WalnutGame from "@/components/WalnutGame";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <WalnutGame />
-    </main>
-  );
-}
